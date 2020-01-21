2c3407081346070432c89a348995793f2db49deb
Lucas Rezende Tedeschi <lucas.tedeschi@onlineapp.com.br>
2020-01-21 10:57:44 -0300

    Upload inicial

---
+++ src/ProjetoTesteTopShelf/Infrastructure/ServiceBaseLifetime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ProjetoTesteTopShelf.Infrastructure
+{
+    public class ServiceBaseLifetime : ServiceControl
+    {
+        private Timer _timer;
+
+        public bool Start(HostControl hostControl)
+        {
+            _timer = new Timer(async c => await ExecuteAsync(), null, TimeSpan.Zero, TimeSpan.FromSeconds(60));
+
+            return true;
+        }
+
+        private async Task ExecuteAsync()
+        {
+            try
+            {
+                _timer.Change(Timeout.Infinite, Timeout.Infinite);
+                //IInício da execução da lógica 
+            }
+            finally
+            {
+                _timer.Change(TimeSpan.FromSeconds(60), TimeSpan.FromSeconds(60));
+            }
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/ProjetoTesteTopShelf/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProjetoTesteTopShelf.Infrastructure;
+using Topshelf;
+
+namespace ProjetoTesteTopShelf
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder().
+                UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureServices((hostBuilderContext, services) =>
+                {
+                    services.AddSingleton(typeof(ServiceBaseLifetime));
+                });
+
+            HostFactory.Run(x =>
+            {
+                x.Service<ServiceBaseLifetime>(sc =>
+                {
+                    sc.ConstructUsing(s => host.Build().Services.GetRequiredService<ServiceBaseLifetime>());
+                    sc.WhenStarted((s, c) => s.Start(c));
+                    sc.WhenStopped((s, c) => s.Stop(c));
+                });
+
+                x.RunAsLocalSystem()
+                    .DependsOnEventLog()
+                    .StartAutomatically()
+                    .EnableServiceRecovery(rc => rc.RestartService(1));
+
+                x.SetDescription("Teste do TopShelf");
+                x.SetDisplayName("Projeto Teste TopShelf");
+                x.SetServiceName("ProjetoTeste.TopShelf");
+            });
+
+            await Task.CompletedTask;
+        }
+    }
+}
